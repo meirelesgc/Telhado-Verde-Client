3a04dd188a12092738e5a97ff80d9c09bfe7f15b
meirelesgc <geu_costa@outlook.com>
2026-05-03 20:15:07 -0300

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,19 @@ import Dashboard from './pages/Dashboard';
 import Configuracoes from './pages/Configuracoes';
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                {/* Rota Pai que contém o Layout */}
-                <Route path="/" element={<MainLayout />}>
+    return <BrowserRouter>
+        <Routes>
+            {/* Rota Pai que contém o Layout */}
+            <Route path="/" element={<MainLayout />}>
+                {/* Rotas Filhas (Renderizadas dentro do Outlet do MainLayout) */}
+                <Route index element={<Dashboard />} /> {/* Caminho padrão "/" */}
+                <Route path="configuracoes" element={<Configuracoes />} />
+                {/* Rota de erro 404 (Opcional) */}
+                <Route path="*" element={<div>Página não encontrada</div>} />
 
-                    {/* Rotas Filhas (Renderizadas dentro do Outlet do MainLayout) */}
-                    <Route index element={<Dashboard />} /> {/* Caminho padrão "/" */}
-                    <Route path="configuracoes" element={<Configuracoes />} />
-
-                    {/* Rota de erro 404 (Opcional) */}
-                    <Route path="*" element={<div>Página não encontrada</div>} />
-
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    );
+            </Route>
+        </Routes>
+    </BrowserRouter>;
 }
 
 export default App;
